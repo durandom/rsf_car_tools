4d713e3ced0848aab27d5c2972b522280594ef6d
Marcel Hild <hild@b4mad.net>
2024-12-05 14:11:44 +0100

    feat: Add JSON augmentation and verbose logging to RSF car configuration

--- power_poly.py
+++ power_poly.py
@@ -1,9 +1,16 @@
 import os
+import json
 import argparse
 from io import StringIO
 from configobj import ConfigObj
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Optional
 from loguru import logger
+
+def setup_logging(verbose: bool) -> None:
+    """Configure logging level based on verbosity"""
+    logger.remove()  # Remove default handler
+    level = "DEBUG" if verbose else "INFO"
+    logger.add(sink=lambda msg: print(msg), level=level)
 
 class Rsf:
     def __init__(self, rsf_path: str):
@@ -22,6 +29,7 @@ class Rsf:
         self._validate_files()
         self.cars: Dict[str, Car] = {}
         self._load_cars()
+        self._augment_cars_with_json()
 
     def _validate_files(self) -> None:
         """Validate all required files exist"""
@@ -54,6 +62,30 @@ class Rsf:
             logger.error(f"Error parsing {file}: {str(e)}")
             exit(1)
 
+    def _augment_cars_with_json(self) -> None:
+        """Load cars.json and add data to existing Car objects"""
+        try:
+            with open(self.cars_json, 'r', encoding='utf-8') as f:
+                cars_json = json.load(f)
+                for car_json in cars_json:
+                    car_id = car_json['id']
+                    if car_id in self.cars:
+                        car = self.cars[car_id]
+                        # Add json attributes to existing Car object
+                        car.path = car_json.get('path', '')
+                        car.hash = car_json.get('hash', '')
+                        car.carmodel_id = car_json.get('carmodel_id', '')
+                        car.user_id = car_json.get('user_id', '')
+                        car.base_group_id = car_json.get('base_group_id', '')
+                        car.ngp = car_json.get('ngp', '')
+                        car.custom_setups = car_json.get('custom_setups', '')
+                        car.rev = car_json.get('rev', '')
+                        car.audio = car_json.get('audio')
+                        car.audio_hash = car_json.get('audio_hash', '')
+                        logger.debug(f"Added JSON data to car {car_id}")
+        except Exception as e:
+            logger.error(f"Error loading cars.json: {str(e)}")
+
     def _load_cars(self) -> None:
         """Load car configurations from personal.ini"""
         config = self.config_parser(self.personal_ini)
@@ -78,7 +110,7 @@ class Car:
 
         Args:
             id (str): Car ID number
-            data (Dict[str, str]): Car configuration data
+            data (Dict[str, str]): Car configuration data from personal.ini
         """
         self.id = id
         self.name = data.get('name', '')
@@ -86,11 +118,25 @@ class Car:
         self.ffb_gravel = int(data.get('forcefeedbacksensitivitygravel', 0))
         self.ffb_snow = int(data.get('forcefeedbacksensitivitysnow', 0))
 
+        # These will be populated from cars.json later
+        self.path = ''
+        self.hash = ''
+        self.carmodel_id = ''
+        self.user_id = ''
+        self.base_group_id = ''
+        self.ngp = ''
+        self.custom_setups = ''
+        self.rev = ''
+        self.audio = None
+        self.audio_hash = ''
+
 def main():
     parser = argparse.ArgumentParser(description='Modify RSF power polygon settings')
     parser.add_argument('rsf_path', help='Path to RSF installation directory')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Enable debug logging')
 
     args = parser.parse_args()
+    setup_logging(args.verbose)
 
     try:
         rsf = Rsf(args.rsf_path)
